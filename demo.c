3a8c3e7fb038667142cdc425412b6a0ca906698a
massoudasadi <massoud.asadi@hotmail.com>
2016-09-24 18:58:44 +0330

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,15 +1,15 @@
 #include<stdio.h> //For standard things
-#include<stdlib.h>    //malloc
-#include<string.h>    //strlen
+#include<stdlib.h> //malloc
+#include<string.h> //strlen
 #include<errno.h>
 #include<netdb.h>
 #include<netinet/in.h>
-#include<netinet/ip_icmp.h>   //Provides declarations for icmp header
-#include<netinet/udp.h>   //Provides declarations for udp header
-#include<netinet/tcp.h>   //Provides declarations for tcp header
-#include<netinet/ip.h>    //Provides declarations for ip header
-#include<netinet/if_ether.h>  //For ETH_P_ALL
-#include<net/ethernet.h>  //For ether_header
+#include<netinet/ip_icmp.h> //Provides declarations for icmp header
+#include<netinet/udp.h> //Provides declarations for udp header
+#include<netinet/tcp.h> //Provides declarations for tcp header
+#include<netinet/ip.h> //Provides declarations for ip header
+#include<netinet/if_ether.h> //For ETH_P_ALL
+#include<net/ethernet.h> //For ether_header
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<sys/ioctl.h>
