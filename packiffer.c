0de323f2c61f0074944b06a6dd2dfbd94f9b2071
Massoud Asadi <massoud.asadi@hotmail.com>
2016-10-15 17:45:48 +0330

    Update packiffer.c

--- packiffer.c
+++ packiffer.c
@@ -10,11 +10,11 @@
 #include <netinet/ip.h> //Provides declarations for ip header
 #include <netinet/if_ether.h> //For ETH_P_ALL
 #include <net/ethernet.h> //For ether_header
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/ioctl.h>
 #include <sys/time.h>
-#include <sys/types.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <pcap.h>
