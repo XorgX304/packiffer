53e69db7ec3aae8b0ab818b2178778c18d7d8146
Massoud Asadi <massoud.asadi@hotmail.com>
2016-10-15 18:04:57 +0330

    update FreeBSD header errors
    errors are gone but warnings remain

--- packiffer.c
+++ packiffer.c
@@ -3,20 +3,20 @@
 #include <string.h> //strlen
 #include <errno.h>
 #include <netdb.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
-#include <netinet/ip_icmp.h> //Provides declarations for icmp header
 #include <netinet/udp.h> //Provides declarations for udp header
 #include <netinet/tcp.h> //Provides declarations for tcp header
 #include <netinet/ip.h> //Provides declarations for ip header
+#include <netinet/ip_icmp.h> //Provides declarations for icmp header
 #include <netinet/if_ether.h> //For ETH_P_ALL
 #include <net/ethernet.h> //For ether_header
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <sys/ioctl.h>
-#include <sys/time.h>
-#include <ctype.h>
-#include <unistd.h>
 #include <pcap.h>
 #include <arpa/inet.h>
 #include <pthread.h>
