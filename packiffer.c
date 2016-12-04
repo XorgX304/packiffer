8c1939b6f2effc437ffc9b43be0acaead216a492
Massoud Asadi <massoud.asadi@hotmail.com>
2016-12-04 21:09:28 +0330

    Update packiffer.c

--- packiffer.c
+++ packiffer.c
@@ -1,6 +1,6 @@
-#include <stdio.h> //For standard things
-#include <stdlib.h> //malloc
-#include <string.h> //strlen
+#include <stdio.h> // For standard things
+#include <stdlib.h> // malloc
+#include <string.h> // strlen
 #include <errno.h>
 #include <netdb.h>
 #include <ctype.h>
@@ -10,12 +10,12 @@
 #include <sys/time.h>
 #include <sys/ioctl.h>
 #include <netinet/in.h>
-#include <netinet/udp.h> //Provides declarations for udp header
-#include <netinet/tcp.h> //Provides declarations for tcp header
-#include <netinet/ip.h> //Provides declarations for ip header
-#include <netinet/ip_icmp.h> //Provides declarations for icmp header
-#include <netinet/if_ether.h> //For ETH_P_ALL
-#include <net/ethernet.h> //For ether_header
+#include <netinet/udp.h> // Provides declarations for udp header
+#include <netinet/tcp.h> // Provides declarations for tcp header
+#include <netinet/ip.h> // Provides declarations for ip header
+#include <netinet/ip_icmp.h> // Provides declarations for icmp header
+#include <netinet/if_ether.h> // For ETH_P_ALL
+#include <net/ethernet.h> // For ether_header
 #include <arpa/inet.h>
 #include <pcap.h> // libpcap library
 #include <arpa/inet.h>
