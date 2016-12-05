68beace195035e7a10afb4c2b019a023f98ccae0
Massoud Asadi <massoud.asadi@hotmail.com>
2016-12-05 19:42:27 +0330

    Update packiffer.c

--- packiffer.c
+++ packiffer.c
@@ -27,9 +27,9 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include <stdio.h> // For standard things
-#include <stdlib.h> // malloc
-#include <string.h> // strlen
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
 #include <errno.h>
 #include <netdb.h>
 #include <ctype.h>
@@ -39,17 +39,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sys/time.h>
 #include <sys/ioctl.h>
 #include <netinet/in.h>
-#include <netinet/udp.h> // Provides declarations for udp header
-#include <netinet/tcp.h> // Provides declarations for tcp header
-#include <netinet/ip.h> // Provides declarations for ip header
-#include <netinet/ip_icmp.h> // Provides declarations for icmp header
-#include <netinet/if_ether.h> // For ETH_P_ALL
-#include <net/ethernet.h> // For ether_header
+#include <netinet/udp.h> 
+#include <netinet/tcp.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
 #include <arpa/inet.h>
-#include <pcap.h> // libpcap library
+#include <pcap.h>
 #include <arpa/inet.h>
-#include <pthread.h> // POSIX threads
-#include <syslog.h> // log
+#include <pthread.h>
+#include <syslog.h>
 
 // command line argument help
 void displayhelp(){
