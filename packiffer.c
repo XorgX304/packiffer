32ca5fd7675d7333f7b81afd9885e8c1c888fb8b
Massoud Asadi <massoud.asadi@hotmail.com>
2018-04-18 21:30:10 +0430

    Update packiffer.c

--- packiffer.c
+++ packiffer.c
@@ -206,7 +206,29 @@ int main(int argc, char **argv){
                pacint.arg = atoi(optarg);
                break;
             case '?':
+               if(optopt == 'l'){
+			char error[PCAP_ERRBUF_SIZE];
+    			pcap_if_t *interfaces,*temp;
+    			int i=0;
+    			if(pcap_findalldevs(&interfaces,error)==-1)
+    			{
+        			printf("\nerror in pcap findall devs");
+        			return -1;    
+    			}
+
+    				printf("\nthe interfaces present on the system are:");
+    			for(temp=interfaces;temp;temp=temp->next)
+    			{
+       				printf("\n%d  :  %s",i++,temp->name);
+        
+    			}
+    			printf("\n");
+    			exit(1);
+
+	       }
+	       else{
                displayhelp();
+               }
             default:
                displayhelp();
         }
